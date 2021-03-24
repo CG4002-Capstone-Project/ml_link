fd5ed0bbda2af48bf0870bab60827c3edf6c7b7f
Mohamed Riyas <42403047+Riyas97@users.noreply.github.com>
2021-03-24 17:31:23 +0800

    intcomm with checksum using accelration and gyro

--- intcomm.py
+++ intcomm.py
@@ -1,15 +1,26 @@
+import serial
 import os
 import time
+#from datacollector import DataCollector
 
-import serial
+#SERIAL_PORT = os.environ['SERIAL_PORT']
+SERIAL_PORT = '/dev/ttyACM0'
 
-from datacollector import DataCollector
+def check(line):
+        try:
+            gyrx, gyry, gyz, accx, accy, accz, cksum = line.split(" ")
+            val = int(gyrx) ^ int(gyry) ^ int(gyz) ^ int(accx) ^ int(accy) ^ int(accz)    
+            newline = ""
+            if (val == int(cksum)):
+                newline = gyrx + " " + gyry + " " + gyz + " " + accx + " " + accy + " " + accz
+            return newline                
+        except Exception as e:
+            print (e)
+            return newline
 
-SERIAL_PORT = os.environ.get("SERIAL_PORT", "/dev/ttyACM0")
 
-
-class IntComm:
-    def __init__(self, serial_port, dancer=1, dashboard=False):
+class IntComm():
+    def __init__(self, serial_port, dancer = 1):
         self.ser = serial.Serial(serial_port, 115200)
         self.ser.flushInput()
         print("Opened serial port %s" % serial_port)
@@ -27,10 +38,8 @@ class IntComm:
             print(line)
 
         # for dashboard
-        self.dashboard = dashboard
-        if self.dashboard:
-            self.datacollector = DataCollector("localhost", 8086, "admin", "xilinx123")
-            self.dancer = dancer
+        #self.datacollector = DataCollector("localhost", 8086, "admin", "xilinx123")
+        self.dancer = dancer
 
     def get_line(self):
         line = self.ser.readline()
@@ -40,18 +49,25 @@ class IntComm:
             return self.get_line()
 
         # status messages; print and get another line
-        if line[0] == "!":
+        if line[0] == '!':
             print(line[1:])
             return self.get_line()
 
         # acc/gyr data messages
-        if line[0] == "#":
-            return line[1:]
+        if line[0] == '#':
+            newline = check(line[1:])
+            if (newline != ""):
+                print ("checksum passed")
+                return newline
+            else:
+                print ("checksum failed")
+                return self.get_line()
 
         print("Invalid message")
         print(line)
 
         return self.get_line()
+
 
     # helper function when you don't need any other data (they are ignored)
     def get_acc_gyr_data(self):
@@ -59,15 +75,11 @@ class IntComm:
         tokens = line.split(" ")
 
         data = [float(token) for token in tokens]
-        if self.dashboard:
-            self.datacollector.insert_gyr_data(
-                int(time.time()), self.dancer, data[0], data[1], data[2]
-            )
-            self.datacollector.insert_acc_data(
-                int(time.time()), self.dancer, data[3], data[4], data[5]
-            )
+        #self.datacollector.insert_gyr_data(int(time.time()), self.dancer, data[0], data[1], data[2])
+        #self.datacollector.insert_acc_data(int(time.time()), self.dancer, data[3], data[4], data[5])
 
         return data
+
 
 
 if __name__ == "__main__":
@@ -84,5 +96,6 @@ if __name__ == "__main__":
 
         if count % 150 == 0:
             end = time.time()
-            print("Receiving data at %d Hz" % round(150 / (end - start)))
+            print("Receiving data at %d Hz" % round(150/(end-start)))
             start = time.time()
+
