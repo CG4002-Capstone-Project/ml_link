6989a9d040e65633bc84b3f4613a2c1b866056da
Brandon Ng <nwjbrandon@gmail.com>
2021-03-25 14:59:58 +0800

    Inference for dance positon and moves (#1)
    * fix deboucning issue
    
    * working code

--- visualize.py
+++ visualize.py
@@ -4,15 +4,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-from inference import Inference
+from inference import Inference, load_model
 
 
-def load_data(file_name="data.csv"):
-    df = pd.read_csv(file_name, sep=",", header=None)
-    df.columns = ["yaw", "pitch", "roll", "accx", "accy", "accz"]
-    df["accx"] = df["accx"] / 8192
-    df["accy"] = df["accy"] / 8192
-    df["accz"] = df["accz"] / 8192
+def load_data(file_name, normalized=True):
+    df = pd.read_csv(file_name, sep=",")
+    if normalized:
+        df["ax"] = df["ax"] / 8192
+        df["ay"] = df["ay"] / 8192
+        df["az"] = df["az"] / 8192
+        df["gx"] = df["gx"] / 100
+        df["gy"] = df["gy"] / 100
+        df["gz"] = df["gz"] / 100
     df["timestamp"] = [i for i in range(len(df))]
     print(df.head())
     print(df.describe())
@@ -24,35 +27,40 @@ def visualize_data(df):
 
     fig, ax = plt.subplots(1, 2)
 
-    sns.scatterplot(data=df, x="timestamp", y="accx", label="accx", ax=ax[0])
-    sns.scatterplot(data=df, x="timestamp", y="accy", label="accy", ax=ax[0])
-    sns.scatterplot(data=df, x="timestamp", y="accz", label="accz", ax=ax[0])
+    sns.scatterplot(data=df, x="timestamp", y="ax", label="ax", ax=ax[0])
+    sns.scatterplot(data=df, x="timestamp", y="ay", label="ay", ax=ax[0])
+    sns.scatterplot(data=df, x="timestamp", y="az", label="az", ax=ax[0])
     ax[0].set(xlabel="readings", ylabel="acceleration")
     ax[0].legend()
 
-    sns.scatterplot(data=df, x="timestamp", y="yaw", label="yaw", ax=ax[1])
-    sns.scatterplot(data=df, x="timestamp", y="pitch", label="pitch", ax=ax[1])
-    sns.scatterplot(data=df, x="timestamp", y="roll", label="roll", ax=ax[1])
-    ax[1].set(xlabel="readings", ylabel="angle")
+    sns.scatterplot(data=df, x="timestamp", y="gx", label="gx", ax=ax[1])
+    sns.scatterplot(data=df, x="timestamp", y="gy", label="gy", ax=ax[1])
+    sns.scatterplot(data=df, x="timestamp", y="gz", label="gz", ax=ax[1])
+    ax[1].set(xlabel="readings", ylabel="gyroscope")
     ax[1].legend()
 
     plt.show()
 
 
 def inference_data(df, verbose):
-    inference = Inference(verbose)
+    model_path = "/home/nwjbrandon/models/dnn_model.pth"
+    scaler_path = "/home/nwjbrandon/models/dnn_std_scaler.bin"
+    model_type = "dnn"
+    model, scaler = load_model(model_type, model_path, scaler_path)
+
+    inference = Inference(model, model_type, scaler, verbose)
 
     for timestamp in range(df.shape[0]):
         data = df.iloc[timestamp]
-        yaw, pitch, roll, accx, accy, accz = (
-            data.yaw,
-            data.pitch,
-            data.roll,
-            data.accx,
-            data.accy,
-            data.accz,
+        gx, gy, gz, ax, ay, az = (
+            data.gx,
+            data.gy,
+            data.gz,
+            data.ax,
+            data.ay,
+            data.az,
         )
-        inference.append_readings(yaw, pitch, roll, accx, accy, accz)
+        inference.append_readings(gx, gy, gz, ax, ay, az)
         result = inference.infer()
         if result:
             print(f"timestamp: {timestamp} result: {result}")
@@ -60,8 +68,10 @@ def inference_data(df, verbose):
 
 def main():
     parser = argparse.ArgumentParser(description="Internal Comms")
-    parser.add_argument("--verbose", default=True, help="verbose", type=bool)
-    parser.add_argument("--visualize", default=True, help="svc or dnn model")
+    parser.add_argument("--verbose", default=False, help="verbose", type=bool)
+    parser.add_argument(
+        "--visualize", default=False, help="file name",
+    )
 
     args = parser.parse_args()
     verbose = args.verbose
@@ -70,11 +80,11 @@ def main():
     print("verbose:", verbose)
     print("visualize:", visualize)
 
-    df = load_data()
-
-    if verbose:
+    if visualize:
+        df = load_data(visualize)
         visualize_data(df)
 
+    df = load_data(visualize, normalized=False)
     inference_data(df, verbose)
 
 
