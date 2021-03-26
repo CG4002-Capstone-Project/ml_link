9231a48b742f225197c722818fbd5a87583ac8f6
Mohamed Riyas <42403047+Riyas97@users.noreply.github.com>
2021-03-26 18:27:18 +0800

    extcomm with emg

--- extcomm.py
+++ extcomm.py
@@ -42,6 +42,8 @@ class Client(threading.Thread):
         self.intcomm = IntComm(serial_port, dancer_id)
 
         # Create a TCP/IP socket and bind to port
+        
+        
         self.shutdown = threading.Event()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_address = (ip_addr, port_num)
@@ -49,6 +51,8 @@ class Client(threading.Thread):
         print("Start connecting>>>>>>>>>>>>")
         self.socket.connect(server_address)
         print("Connected")
+        
+        
 
     def run(self):
 
@@ -65,24 +69,46 @@ class Client(threading.Thread):
                 start = time.time()
             count += 1
             data = self.intcomm.get_line()
-            message_final = (
-                str(dancer_id) + "|" + str(t1) + "|" + str(offset) + "|" + data + "|"
-            )
-            database_msg = str(dancer_id) + "|" + str(t1) + "|" + data + "|"
-            channel.basic_publish(
-                exchange="", routing_key=DB_QUEUES[dancer_id], body=database_msg
-            )
-            print("Sending", message_final)
-
             t1 = time.time()
-            self.send_message(message_final)
-            timestamp = self.receive_timestamp()
-            t4 = time.time()
-            t2 = float(timestamp.split("|")[0][:18])
-            t3 = float(timestamp.split("|")[1][:18])
-            RTT = t4 - t3 + t2 - t1
-            offset = (t2 - t1) - RTT / 2
+            # IMU data
+            if (data[0] == "#"):
+                data = data[1:]
+                gyrx, gyry, gyrz, accx, accy, accz, cksum = data.split(" ")
+                data = gyrx + " " + gyry + " " + gyrz + " " + accx + " " + accy + " " +accz
+                message_final = (
+                    str(dancer_id) + "|" + str(t1) + "|" + str(offset) + "|" + data + "|"
+                )
+                database_msg = str(dancer_id) + "|" + str(t1) + "|" + data + "|"
+                channel.basic_publish(
+                    exchange="", routing_key=DB_QUEUES[dancer_id], body=database_msg
+                )
+                print("Sending IMU", message_final)
+                t1 = time.time()
+                
+                self.send_message(message_final)
+                timestamp = self.receive_timestamp()
+                t4 = time.time()
+                t2 = float(timestamp.split("|")[0][:18])
+                t3 = float(timestamp.split("|")[1][:18])
+                RTT = t4 - t3 + t2 - t1
+                offset = (t2 - t1) - RTT / 2
+                
+                
+            # EMG data
+            elif (data[0] == "$"):
+                if (emg is True):
+                    data = data[1:]
+                    mav, rms, freq, cksum = data.split(" ")
+                    data = mav + " " + rms + " " + freq
+                    message_emg = (
+                        str(t1) + "|" + data
+                    )
+                    print("Sending EMG", message_emg)
+                    channel.basic_publish(
+                        exchange="", routing_key='emg', body=message_emg
+                    )
 
+            
     # To encrypt the message, which is a string
     def encrypt_message(self, message):
         raw_message = "#" + message
@@ -124,9 +150,11 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="External Comms")
     parser.add_argument("--dancer_id", help="dancer id", type=int, required=True)
+    parser.add_argument("--emg", default=False, help="collect emg data", type=bool)
 
     args = parser.parse_args()
     dancer_id = args.dancer_id
+    emg = args.emg
 
     CLOUDAMQP_URL = "amqps://yjxagmuu:9i_-oo9VNSh5w4DtBxOlB6KLLOMLWlgj@mustang.rmq.cloudamqp.com/yjxagmuu"
     params = pika.URLParameters(CLOUDAMQP_URL)
@@ -136,6 +164,16 @@ if __name__ == "__main__":
     channel = connection.channel()
     channel.queue_declare(queue=DB_QUEUES[dancer_id])
 
+    if (emg is True):
+        CLOUDAMQP_URL = "amqps://yjxagmuu:9i_-oo9VNSh5w4DtBxOlB6KLLOMLWlgj@mustang.rmq.cloudamqp.com/yjxagmuu"
+        params = pika.URLParameters(CLOUDAMQP_URL)
+        params.socket_timeout = 5
+
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+        channel.queue_declare(queue='emg')
+
+    
     ip_addr = "127.0.0.1"
     port_num = PORT_NUM[dancer_id]
     group_id = "18"
