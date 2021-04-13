daf43c41798d34a81d75635d2c5aaecd1c3b3861
nwjbrandon <nwjbrandon@gmail.com>
2021-04-13 17:46:59 +0800

    update swap positions

--- ultra96.py
+++ ultra96.py
@@ -167,28 +167,62 @@ class ServerFactory(Factory):
 
 
 def swap_positions(positions, pos):
-    if pos == ["S", "S", "S"]:
+    pos = [pos[positions[0]], pos[positions[1]], pos[positions[2]]]
+
+    if pos == ["S", "S", "S"]:  # 1
         return [positions[0], positions[1], positions[2]]
-    elif pos == ["R", "L", "S"]:
+    elif pos == ["S", "S", "L"]:
+        return [positions[0], positions[1], positions[2]]
+    elif pos == ["S", "S", "R"]:
+        return [positions[0], positions[1], positions[2]]
+    elif pos == ["S", "L", "S"]:
         return [positions[1], positions[0], positions[2]]
-    elif pos == ["R", "S", "L"]:
-        return [positions[2], positions[1], positions[0]]
-    elif pos == ["S", "R", "L"]:
-        return [positions[0], positions[2], positions[1]]
-    elif pos == ["R", "R", "L"]:
-        return [positions[2], positions[0], positions[1]]
-    elif pos == ["R", "L", "L"]:
+    elif pos == ["S", "L", "L"]:
         return [positions[1], positions[2], positions[0]]
-    else:
-        # handle invalid cases
-        if pos == ["S", "L", "L"]:
-            case1 = [positions[1], positions[2], positions[0]]
-            return case1
-        elif pos == ["R", "R", "S"]:
-            case1 = [positions[2], positions[0], positions[1]]
-            return case1
-        else:
-            return [positions[0], positions[1], positions[2]]
+    elif pos == ["S", "L", "R"]:
+        return [positions[1], positions[0], positions[2]]
+    elif pos == ["S", "R", "S"]:
+        return [positions[0], positions[1], positions[2]]
+    elif pos == ["S", "R", "L"]:  # 2
+        return [positions[0], positions[2], positions[1]]
+    elif pos == ["S", "R", "R"]:
+        return [positions[0], positions[2], positions[1]]
+    elif pos == ["L", "S", "S"]:
+        return [positions[0], positions[1], positions[2]]
+    elif pos == ["L", "S", "L"]:
+        return [positions[2], positions[1], positions[0]]
+    elif pos == ["L", "S", "R"]:
+        return [positions[0], positions[1], positions[2]]
+    elif pos == ["L", "L", "S"]:
+        return [positions[1], positions[0], positions[2]]
+    elif pos == ["L", "L", "L"]:
+        return [positions[1], positions[2], positions[0]]
+    elif pos == ["L", "L", "R"]:
+        return [positions[2], positions[0], positions[1]]
+    elif pos == ["L", "R", "S"]:
+        return [positions[2], positions[0], positions[1]]
+    elif pos == ["L", "R", "L"]:
+        return [positions[2], positions[0], positions[1]]
+    elif pos == ["L", "R", "R"]:
+        return [positions[2], positions[0], positions[1]]
+    elif pos == ["R", "S", "S"]:
+        return [positions[0], positions[1], positions[2]]
+    elif pos == ["R", "S", "L"]:  # 3
+        return [positions[2], positions[1], positions[0]]
+    elif pos == ["R", "S", "R"]:
+        return [positions[1], positions[0], positions[2]]
+    elif pos == ["R", "L", "S"]:  # 4
+        return [positions[1], positions[0], positions[2]]
+    elif pos == ["R", "L", "L"]:  # 5
+        return [positions[1], positions[2], positions[0]]
+    elif pos == ["R", "L", "R"]:
+        return [positions[1], positions[0], positions[2]]
+    elif pos == ["R", "R", "S"]:
+        return [positions[2], positions[0], positions[1]]
+    elif pos == ["R", "R", "L"]:  # 6
+        return [positions[2], positions[0], positions[1]]
+    elif pos == ["R", "R", "R"]:
+        return [positions[2], positions[0], positions[1]]
 
 
 def format_results(positions, dance_moves, pos, sync_delay):
